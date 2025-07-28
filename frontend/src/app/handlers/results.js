278ab511e0deadf43dd51702f60a736a1095caa5
Nông Quốc Khánh <56966634+hkgkhanh@users.noreply.github.com>
2025-07-28 09:50:03 +0000

    api route for wca rankings

---
+++ frontend/src/app/handlers/results.js
@@ -0,0 +1,13 @@
+export async function getWcaRankings(type, event, person_or_result) {
+    const res = await fetch('/api/results/wca-rankings', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ type, event, person_or_result })
+    });
+
+    const data = await res.json();
+    console.log(data);
+    return data;
+}
