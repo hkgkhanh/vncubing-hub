e72f0e391043f100b704527f43f324b0693b0a47
Nông Quốc Khánh <hkg11khanh@gmail.com>
2025-08-04 15:00:03 +0700

    responsive header

--- frontend/src/app/_layouts/header/Index.js
+++ frontend/src/app/_layouts/header/Index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import React, { useState, useEffect } from 'react';
 import '../../_styles/header/default.css';
 import navbarData from '../../../data/header.json';
 import LogoutButton from '@/app/_components/LogoutButton';
@@ -10,15 +12,41 @@ function Header({ isLoggedIn }) {
     const authLinks = navbarData['auth'];
     const logoData = navbarData['logo'];
 
+    const [navbarIsOpen, setNavbarIsOpen] = useState(true);
+
+    useEffect(() => {
+        const updateNavVisibility = () => {
+            const isLargeScreen = window.matchMedia("(min-width: 992px)").matches;
+            setNavbarIsOpen(isLargeScreen);
+        };
+
+        updateNavVisibility();
+
+        window.addEventListener("resize", updateNavVisibility);
+
+        return () => {
+            window.removeEventListener("resize", updateNavVisibility);
+        };
+    }, []);
+
+    const handleToggleNav = () => {
+        if (window.innerWidth < 992) {
+        setNavbarIsOpen((prev) => !prev);
+        }
+    };
+
     return (
         <div className="navbar" id='navbar'>
             <div className="logo">
                 <a href="/">
                     <img src={logoData.image} alt={logoData.alt} id="logo" />
                 </a>
+                <a href="#" className='toggle-navbar' onClick={handleToggleNav}>
+                    <img src={`/ui/list.svg`} alt="" />
+                </a>
             </div>
             
-            <div className="nav-links">
+            <div className={`nav-links ${navbarIsOpen ? "open" : ""}`}>
                 {navLinks.map((item, index) => (
                     <a href={item.link} key={index}>
                         {lang === 'vi' ? item.label_vi : item.label_en}
@@ -31,11 +59,11 @@ function Header({ isLoggedIn }) {
                     <option value="en">English</option>
                 </select> */}
                 {isLoggedIn ? (
-                    <div className="auth">
+                    <div className={`auth ${navbarIsOpen ? "open" : ""}`}>
                         <LogoutButton />
                     </div>
                 ) : (
-                    <div className="auth">
+                    <div className={`auth ${navbarIsOpen ? "open" : ""}`}>
                         <a href={authLinks.login.link}>
                             {lang === 'vi' ? authLinks.login.label_vi : authLinks.login.label_en}
                         </a>
